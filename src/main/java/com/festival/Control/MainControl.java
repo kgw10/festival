6472d85d6464731bcd58d034c4a56e25f595fa64
kgw10 <aa01059183132@gmail.com>
2024-11-01 17:30:41 +0900

    1111

---
+++ src/main/java/com/festival/Control/MainControl.java
@@ -0,0 +1,17 @@
+package com.festival.Control;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainControl {
+
+    @GetMapping("/")
+    public String head(){
+
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/festival/Control/MemberController.java
@@ -0,0 +1,21 @@
+package com.festival.Control;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/signIn")
+    public String loginPage(Model model){
+
+
+
+        return "member/login";
+    }
+}
